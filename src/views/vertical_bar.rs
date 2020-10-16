42ce6b4b882a96e7001ed466baf6aa1df9968472
Jeremiah Russell <jrussell@jerus.ie>
2020-10-16 08:32:54 +0100

    [lint] writing `&Vec<_>` instead of `&[_]`
    Problem
    warning: writing `&Vec<_>` instead of `&[_]` involves one more
     reference and cannot be used with non-Vec-based slices.
       --> src/views/vertical_bar.rs:203:27
        |
    203 |     fn extract_keys(data: &Vec<impl BarDatum>) -> Vec<String> {
        |                           ^^^^^^^^^^^^^^^^^^^ help: change this
        to: `&[impl BarDatum]`
        |
        = help: for further information visit https://rust-lang.github.io/
        rust-clippy/master/index.html#ptr_arg
    Solution
    Implement recommended solution
    Note

--- src/views/vertical_bar.rs
+++ src/views/vertical_bar.rs
@@ -200,7 +200,7 @@ impl<'a> VerticalBarView<'a> {
     }
 
     /// Extract the list of keys to use when stacking and coloring the bars.
-    fn extract_keys(data: &Vec<impl BarDatum>) -> Vec<String> {
+    fn extract_keys(data: &[impl BarDatum]) -> Vec<String> {
         let mut keys = Vec::new();
         let mut map = HashMap::new();
 
